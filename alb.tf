aa9f00c051a58a9b61c12836990587b10aa5e720
Suraj Kumar Shrestha <suraj@united-asian.com>
2020-04-18 16:09:22 +0545

    fixed alb security group

--- alb.tf
+++ alb.tf
@@ -14,6 +14,13 @@ resource "aws_security_group" "lb" {
   name   = "${var.swarm_name}-lb-security-group"
   vpc_id = aws_vpc.main.id
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port        = 80
     to_port          = 80
